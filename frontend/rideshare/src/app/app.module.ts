ac31c11a4cac6152619c3b62b946fa1075b5e9fd
SANJEEVAN\Sanjeevan <sanjeevan_sigdel@yahoo.com>
2021-04-10 19:45:31 -0500

    integrated Nebular UI and set up a layout for the home page

--- frontend/rideshare/src/app/app.module.ts
+++ frontend/rideshare/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule, NbStepperModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbSidebarModule.forRoot(),
+    NbLayoutModule,
+    NbStepperModule,
+    NbCardModule,
+    NbButtonModule,
+    NbEvaIconsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
