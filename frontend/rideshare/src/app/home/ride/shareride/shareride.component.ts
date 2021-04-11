ea7893bb5fa40987def8489da769e22b8883c005
ayoubelabbasse <belabsayoub@gmail.com>
2021-04-11 14:51:03 -0500

    ride share componenet

---
+++ frontend/rideshare/src/app/home/ride/shareride/shareride.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shareride',
+  templateUrl: './shareride.component.html',
+  styleUrls: ['./shareride.component.css']
+})
+export class SharerideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
