99a266295e604ffda3d2e81eafd2875be7f3305d
ayoubelabbasse <belabsayoub@gmail.com>
2021-04-12 05:01:27 -0500

    fix

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.set("port", 3000);
+app.set("port", 5000);
 
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
