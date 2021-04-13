710243728e683ba5f5780fbc5e17c1383f3ee9c3
SANJEEVAN\Sanjeevan <sanjeevan_sigdel@yahoo.com>
2021-04-12 20:20:43 -0500

    error fix2

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ var app = express();
 app.set("port", 5000);
 
 mongoose
-.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
