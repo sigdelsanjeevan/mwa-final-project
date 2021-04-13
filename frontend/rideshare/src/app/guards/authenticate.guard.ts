93fc3aeb18afaf3c4a6023d0e2103cd7c05efda2
SANJEEVAN\Sanjeevan <sanjeevan_sigdel@yahoo.com>
2021-04-13 09:01:43 -0500

    added guards part 2

--- frontend/rideshare/src/app/guards/authenticate.guard.ts
+++ frontend/rideshare/src/app/guards/authenticate.guard.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private auth: AuthService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (!localStorage.getItem("username") || localStorage.getItem("username") == "undefined") {
-      this.router.navigateByUrl('auth/login?returnUrl=' + state.url.toString());
+    // if (!localStorage.getItem("username") || localStorage.getItem("username") == "undefined") {
+    // this.router.navigateByUrl('auth/login?returnUrl=' + state.url.toString());
+    // this.router.navigateByUrl('/');
+    // }
+
+    if (!this.auth.isLoggedIn) {
+      alert('You must be logged in to access this page');
+      this.router.navigateByUrl('/login');
     }
     return true;
   }
